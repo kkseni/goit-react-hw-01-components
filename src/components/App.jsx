3e2ba0efd81d00b5e2b90ac3054fc52a10389da2
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2022-09-09 11:38:05 +0300

    ad

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import userEvent from "@testing-library/user-event";
+import user from './user.json'
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
-    </div>
-  );
-};
+
+
+     </div>
+   );
+ };
